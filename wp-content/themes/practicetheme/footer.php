fd77ae41d3c824ce830fbf3691f5987a4422a7c1
Chris Richter <wcrichter@gmail.com>
2017-06-02 12:39:15 -0400

    completed 'WordPress Navigation Menus'

--- wp-content/themes/practicetheme/footer.php
+++ wp-content/themes/practicetheme/footer.php
@@ -1,4 +1,12 @@
       <footer class="site-footer">
+        <nav class="site-nav">
+          <?php
+            $args = array(
+              'theme_location' => 'footer'
+            );
+          ?>
+          <?php wp_nav_menu( $args ); ?>
+        </nav>
         <p>
           <?php bloginfo('name'); ?> - &copy; <?php echo date('Y'); ?>
         </p>
--- wp-content/themes/practicetheme/functions.php
+++ wp-content/themes/practicetheme/functions.php
@@ -4,4 +4,10 @@
   }
 
   add_action('wp_enqueue_scripts', 'wordpressPractice20160601_resources');
+
+  // Navigation Menus
+  register_nav_menus(array(
+    'primary' => __( 'Primary Menu'),
+    'footer' => __( 'Footer Menu'),
+  ));
 ?>
